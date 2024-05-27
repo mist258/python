7ba3d742827615956ebbb24148310d6a57e39d72
Makima3 <natalia.kolchuk@gmail.com>
2024-05-27 20:16:02 +0300

    simple program

--- exercises_py/advanced.py
+++ exercises_py/advanced.py
@@ -1,5 +1,4 @@
 # Створити програму - гру "Поле чудес".
-# Зареєструватись на github, та створити окремий репозиторій для цього завдання
 # 1. Програма буде брати зі списку слів одне рандомне слово.
 # 2. Програма буде отримувати від користувача число - кількість спроб вгадати
 # 3. Далі програма буде чекати від користувача або букву, або ціле слово.
@@ -21,4 +20,60 @@
 # Input:"p"
 # Output:"apple"
 # Output:"Вітаю, ви вгадали слово"
+import random as rn
+
+
+def randoms():
+
+    words_list = ['apple']
+    random_word = rn.choice(words_list)
+
+    return random_word
+
+
+randoms()
+
+
+def wonder_field():
+    random_value = randoms()
+
+    number_of_attempts = int(input('How many attempts do you want? Enter number:'))
+
+    while number_of_attempts:
+
+        word_or_letter = input('Enter a word or letter: ').lower()
+
+        if word_or_letter == random_value:
+            print(f'You guessed {random_value}')
+            break
+        elif len(word_or_letter) > 1 and word_or_letter != random_value:
+            print('Try again')
+            number_of_attempts -= 1
+
+        if len(word_or_letter) == 1:
+            if word_or_letter in random_value:
+                guessed_word.append(word_or_letter)
+                print('You guessed letter ')
+                number_of_attempts -= 1
+            else:
+                number_of_attempts -= 1
+
+        elif number_of_attempts == 0:
+            return 'You lost'
+
+
+guessed_word = []
+
+wonder_field()
+
+
+def word(guessed_word):
+
+    return ''.join(letter if letter in guessed_word else '*' for letter in guessed_word)
+
+
+print(word(guessed_word))
+
+
+
 
