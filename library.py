78175ee13efc6749e42640fbbd1efa07e8a4b7be
Makima3 <natalia.kolchuk@gmail.com>
2024-07-10 20:56:15 +0300

    simple functionality for the library

--- library.py
+++ library.py
@@ -7,15 +7,15 @@
 #Part 2 LIBRARY
 # *******1.Клас Library буде мати поля books, users. # теба створити 2 класи
 # *******2.Методи які дозволять зареєструвати юзера у бібліотеці
-# 3.Методи які дозволять знайти книжку за isbn.
-# 4.Методи які дозволять показати всі доступні книжки у бібліотеці.
+# *******3.Методи які дозволять знайти книжку за isbn.
+# *******4.Методи які дозволять показати всі доступні книжки у бібліотеці.
 #*********
 #Part 3 USER
 # *******1.Клас User повинен мати такі атрибути як name, user_id.
-# 2.Інфа про функціонал для Customer та Employee, де ви самі повинні розібратись які атрибути де мають бути та як це буде виглядати.
+# *******2.Інфа про функціонал для Customer та Employee, де ви самі повинні розібратись які атрибути де мають бути та як це буде виглядати.
 # 3.Атрибути borrowed_books, salary, library.
 # 4.Методи для взяття книжки з бібліотеки, та повернення.
-# 5.Методи для додавання книжки у бібліотеку та видаленню.
+# *******5.Методи для додавання книжки у бібліотеку та видаленню.
 import re
 from abc import ABC
 
@@ -37,6 +37,9 @@ class Book:
         pattern_isbn = r'^(?:\d{3})-(?:\d{1,5})-(?:\d{1,7})-(?:\d{1,7})-(?:\d{1})$' #since 2007 ISBN contains 13 digits
         if not re.fullmatch(pattern_isbn, isbn):
             raise ValueError('Invalid ISBN number')
+
+    def get_book_isbn(self):
+        return self.__isbn
 
     def check_availability(self): # check whether the book is available in library using ISBN
         pass
@@ -94,7 +97,7 @@ class Library(MixinMethodsForManageLibraries):
     def __init__(self):
         self._users: list[User] = [] # list of all users in lib
         self._books: list = [] # list of all books
-        self._borrowed_books: list = []
+        self._borrowed_books: list = [] # show all borrowed books frolm library
 
     def register_user_in_lib(self, customer: User): # DONE
         self.register_user(customer)
@@ -110,7 +113,7 @@ class Library2(MixinMethodsForManageLibraries):
     def __init__(self):
         self._users: list[User] = [] # list of all users
         self._books: list = [] # list of all books
-        self._borrowed_books: list = []
+        self._borrowed_books: list = [] # show all borrowed books from library
 
     def register_user_in_lib2(self, customer: User): # DONE
         self.register_user(customer)
@@ -149,25 +152,46 @@ class Employee(Library, Library2,  Book):
     def __init__(self):
         super().__init__()
         self.borrowed_books: list[Book] = []
+        self.salary = 0
 
     def show_borrowed_books(self): # show all borrowed books ????????????
-        if self.borrowed_books:
-            return '\n'.join([f'Title: {book._title}, Author: {book._author}'for book in self.borrowed_books])
+        if self.borrowed_books is not None:
+            for book in self._borrowed_books:
+                print(book)
         else:
-            return 'No borrowed books'
-
-    def delete_book(self): # delete book from library
-        pass  # using for del ISBN
+            print('No borrowed books')
 
     @staticmethod
-    def find_book(book_isbn, *args):
-        pass
+    def delete_book(book_isbn, *args): # delete book from all libraries #DONE
+        for lib in args:
+            if hasattr(lib, '_books'):
+                for book in lib._books:
+                    if hasattr(book, 'get_book_isbn') and book.get_book_isbn() == book_isbn:
+                        lib._books.remove(book)
+                        print(f'Deleted book: {book}')
+                        break  # delete the fist found book
+                    elif book.get_book_isbn != book_isbn:
+                        raise ValueError(f'Book ISBN {book_isbn} does not exist or invalid')
 
     @staticmethod
-    def add_book(add_book, *args): #DONE
+    def find_book(book_isbn, *args): # finds the book by ISBN in both libraries DONE
+        for lib in args:
+            if hasattr(lib, '_books'):
+                for book in lib._books:
+                    if hasattr(book, 'get_book_isbn') and book.get_book_isbn() == book_isbn:
+                        print(book)
+                        return book
+                    else:
+                        raise ValueError('Book not found')
+
+    @staticmethod
+    def add_book(add_book, *args): # add books to libraries DONE
         for book in args:
             if hasattr(book, '_books'):
                 book._books.append(add_book)
+
+    def employee_salary(self, salary):
+        pass
 
 
 cus = Customer('Anny', 7, 'any.email@gmail.com')
@@ -175,12 +199,16 @@ lib1 = Library()
 lib2 = Library2()
 empl = Employee()
 book1 = Book('Sword of Destiny', 'Andrzej Sapkowski', '978-0-316-27805-7')
+book12 = Book('Sword of Destiny', 'Andrzej Sapkowski', '978-0-316-27805-7')
+book13 = Book('Sword of Destiny', 'Andrzej Sapkowski', '978-0-316-27805-7')
 book2 = Book('Blood of Elves', 'Andrzej Sapkowski', '978-0-575-08636-0')
 book3 = Book('Time of Contempt', 'Andrzej Sapkowski', '978-0-575-08637-7')
 empl.add_book(book1, lib1, lib2)
 empl.add_book(book2, lib2, lib1)
 empl.add_book(book3, lib1, lib2)
-
+empl.add_book(book12, lib1)
+empl.add_book(book13, lib1)
+#print(empl.find_book(book1.get_book_isbn(), lib1, lib2))
 #     book = [
 #     ['Sword of Destiny', 'Andrzej Sapkowski', '978-0-316-27805-7'],
 #     ['Blood of Elves', 'Andrzej Sapkowski', '978-0-575-08636-0'],
